766899bc553a55b54f8e6654cba8b692a86e645a
unknown <nguyenquochuydl123@gmail.com>
2024-03-20 20:26:06 +0700

    Final version

--- Clients/LearningChineseVideo/src/components/footer.js
+++ Clients/LearningChineseVideo/src/components/footer.js
@@ -17,14 +17,23 @@ const HayugoFooter = () => {
                                 src={'/hayugo-logo_landscape.png'} />
                             <Typography
                                 variant="subtitle1">
-                                {`HayuGo - điểm đến lý tưởng cho việc học tiếng Trung thú vị và hiệu quả. Với bộ sưu tập video đa dạng từ HSK 1 đến HSK 5, bạn có thể linh hoạt chọn lựa và bắt đầu hành trình học tiếng Trung của mình. Tính năng tra từ trên phụ đề giúp bạn hiểu rõ ngữ cảnh và nâng cao kỹ năng ngôn ngữ tự tin. Hãy khám phá và tận hưởng sức mạnh của việc học tiếng Trung qua video ngay hôm nay!`}
+                                {`HayuGo - Điểm đến lý tưởng cho việc học tiếng Trung thú vị và hiệu quả. Với bộ sưu tập video đa dạng từ cấp độ HSK 1 đến HSK 5, bao gồm các video mang nội dung giải trí, học thuật và nhiều hơn nữa. `}
+                                <br></br>
+                                <br></br>
+                                {`Các tính năng như tra từ trực tiếp trên phụ đề, linh hoạt chuyển đổi giữa các loại phụ đề, cùng tính năng lưu từ vựng sẽ giúp bạn hiểu rõ hơn và ghi nhớ từ vựng một cách hiệu quả. Theo dõi fanpage của nhóm nghiên cứu để khám phá thêm nhiều thông tin thú vị và cập nhật mới nhất!`}
                             </Typography>
+                            <p>
+                                Fanpage: <span><a>https://s.net.vn/bS4l</a></span>
+                            </p>
+                            <p>
+                                Liên hệ phản hồi chất lượng bản dịch và đóng góp video: <span><a>hayugojhz@gmail.com</a></span>
+                            </p>
                         </Box>
                     </Grid>
                     <Grid
                         sx={{ padding: '25px' }}
                         xl="3.5">
-                        <Box pt="70px">
+                        <Box pt="20px">
                             <Typography
                                 fontSize="20px"
                                 variant="h5">
@@ -32,24 +41,36 @@ const HayugoFooter = () => {
                             </Typography>
                             <Typography
                                 fontSize="14px"
-                                mt="5px"
+                                mt="30px"
                                 variant="subtitle1">
                                 {`Khoa Tiếng Trung Trường Đại Học Sư Phạm TPHCM`}
                             </Typography>
                             <ul style={{ listStylePosition: 'outside' }}>
-                                <li style={{ marginTop: '10px' }}>{`(ảnh) Nguyễn Thị Thùy Giang`}</li>
-                                <li style={{ marginTop: '10px' }}>{`(ảnh) Trần Thị Thùy Trâm`}</li>
-                                <li style={{ marginTop: '10px' }}>{`(ảnh) Nguyễn Hữu Huỳnh`}</li>
+                                <li style={{ marginTop: '10px', color: 'black' }}>
+                                    <a style={{ color: 'black' }} href="https://web.facebook.com/profile.php?id=100034698395094">
+                                        {`Nguyễn Thị Thúy Giang`}
+                                    </a>
+                                </li>
+                                <li style={{ marginTop: '10px', color: 'black' }}>
+                                    <a style={{ color: 'black' }} href="https://www.facebook.com/tranthi.thuytram.543">
+                                        {`Trần Thị Thùy Trâm`}
+                                    </a>
+                                </li>
+                                <li style={{ marginTop: '10px', color: 'black' }}>
+                                    <a  style={{ color: 'black' }} href="https://www.facebook.com/profile.php?id=100011355822706&mibextid=ZbWKwL">
+                                        {`Nguyễn Hữu Huỳnh`}
+                                    </a>
+                                </li>
                             </ul>
                         </Box>
                     </Grid>
                     <Grid
                         sx={{ padding: '25px' }}
                         xl="3.5">
-                        <Box pt="60px">
+                        <Box pt="70px">
                             <Typography
                                 variant="subtitle1">
-                                {`Bản quyền thuộc về nhóm nghiên cứu đề tài “Xây dựng ngữ liệu trực tuyến phục vụ tra cứu học tập, tra cứu tiếng Trung cho người học tiếng Trung ở Việt Nam”`}
+                                {`Bản quyền thuộc về nhóm nghiên cứu đề tài “Xây dựng ngữ liệu trực tuyến phục vụ học tập, tra cứu tiếng Trung cho người học tiếng Trung ở Việt Nam”`}
                             </Typography>
                             <Typography variant="subtitle1" mt="20px">
                                 {`Website được xây dựng bởi Nguyễn Quốc Huy`}
@@ -64,8 +85,12 @@ const HayugoFooter = () => {
                                         <a style={{ color: 'black' }} href="mailto:nguyenquochuydl123@gmail.com">nguyenquochuydl123@gmail.com</a>
                                     </span>
                                 </p>
+                                <p>{`Facebook: `}
+                                    <span style={{ fontWeight: '600' }}>
+                                        <a style={{ color: 'black' }} href="https://www.facebook.com/">Nguyễn Huy</a>
+                                    </span>
+                                </p>
                             </Typography>
-
                         </Box>
                     </Grid>
                 </Grid>

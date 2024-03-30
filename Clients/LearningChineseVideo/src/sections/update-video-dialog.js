313d0d17332ba4d3045fc6f1e3761f91d4a64fb8
unknown <nguyenquochuydl123@gmail.com>
2024-03-30 23:44:54 +0700

    add statging env]

--- Clients/LearningChineseVideo/src/sections/update-video-dialog.js
+++ Clients/LearningChineseVideo/src/sections/update-video-dialog.js
@@ -5,22 +5,114 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useFormik } from 'formik';
-import { Box, Stack, TextField, Typography } from '@mui/material';
-import { useRouter } from 'next/router';
-import { addVoca, editVocabulary } from 'src/services/api/voca-api';
+import { Box, Card, Stack, TextField, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useSnackbar } from 'notistack';
 import { MuiChipsInput } from 'mui-chips-input';
 import { editVideo } from 'src/services/api/video-api';
+import { uploadFile } from 'src/services/api/upload-api';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+const UpdateVttFile = ({
+    vttId, title, inputDomId, onUploaded
+}) => {
+    const [vtt, setVtt] = useState(null);
+    const onPickVttFile = (event) => {
+        var file = event.target.files[0];
+        uploadFile(file)
+            .then(({ medias }) => {
+                const media = medias[0];
+                const data = {
+                    id: vttId,
+                    url: media.url,
+                    filename: media.filename
+                };
+                setVtt(data);
+                onUploaded(data);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <Box>
+            <input
+                onChange={onPickVttFile}
+                style={{ display: "none" }}
+                type="file"
+                multiple
+                accept="text/vtt"
+                id={inputDomId}
+            />
+            {Boolean(vtt)
+                ? <Card
+                    sx={{
+                        width: '400px',
+                        backgroundColor: '#f5f5f5',
+                        textDecoration: 'none',
+                        borderRadius: '10px',
+                        overflow: 'hidden'
+                    }}>
+                    <Stack
+                        spacing="15px"
+                        sx={{
+                            paddingX: '10px',
+                            paddingY: '5px',
+                        }}
+                        direction="row">
+                        <Box
+                            sx={{
+                                bgcolor: 'rgba(3, 172, 19, 0.3)',
+                                alignItems: 'center',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                width: '40px',
+                                height: '40px',
+                                aspectRatio: 1,
+                                borderRadius: '10px',
+                                color: '#03ac13'
+                            }}>
+                            <AssignmentIcon />
+                        </Box>
+                        <Box sx={{ width: '100%' }}>
+                            <Typography
+                                mb="5px"
+                                fontSize="14px"
+                                variant="subtitle2">
+                                {vtt.filename}
+                            </Typography>
+                            <Typography
+                                mb="5px"
+                                fontSize="12px"
+                                color='green'
+                                variant="subtitle2">
+                                Đã tải lên
+                            </Typography>
+                        </Box>
+                        <Button
+                            onClick={() => setVtt(null)}
+                            sx={{ color: '#696969', paddingX: '0px' }}>
+                            <CloseIcon />
+                        </Button>
+                    </Stack>
+                </Card>
+                : <Button
+                    onClick={() => document.getElementById(inputDomId)?.click()}
+                    variant='contained' sx={{ width: '250px' }}>
+                    {title}
+                </Button>
+            }
+
+        </Box>
+    )
+}
 
 
 export default function UpdateVideoDialog({
     open, handleClose, video, onUpdated
 }) {
     const { enqueueSnackbar } = useSnackbar();
-    const router = useRouter();
-
     const resetForm = () => {
         formik.setValues({
             title: '',
@@ -81,6 +173,7 @@ export default function UpdateVideoDialog({
 
     useEffect(() => {
         if (video) {
+            console.log(video);
             formik.setValues({
                 title: video.title,
                 description: video.description,
@@ -149,6 +242,24 @@ export default function UpdateVideoDialog({
                                 }}
                             />
                         </Box>
+                        <Stack
+                            spacing="10px"
+                            mt="20px">
+                            <UpdateVttFile
+                                title={`Cập nhật VTT cách từ`}
+                                // vttId={}
+                                onUploaded={(vtt) => { console.log(vtt) }}
+                                inputDomId="vtt-cach-tu" />
+                            <UpdateVttFile
+                                title={`Cập nhật VTT tiếng Trung`}
+                                inputDomId="vtt-tieng-trung" />
+                            <UpdateVttFile
+                                title={`Cập nhật VTT phiên âm`}
+                                inputDomId="vtt-phien-am" />
+                            <UpdateVttFile
+                                title={`Cập nhật VTT tiếng Việt`}
+                                inputDomId="vtt-tieng-viet" />
+                        </Stack>
                     </Stack>
                 </DialogContent>
                 <DialogActions>
